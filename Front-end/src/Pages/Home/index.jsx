e8ef2229ffb819f17deeeeb844c70e9c8da67999
Juan Machado <juanmachado123@hotmail.com>
2023-12-07 18:43:51 -0300

    enviando

--- Front-end/src/Pages/Home/index.jsx
+++ Front-end/src/Pages/Home/index.jsx
@@ -1,79 +1,118 @@
-import CampoTexto from "../../components/campoTexto"
-import Botao from "../../components/Botao"
-import "./home.css"
 import React, { useState } from 'react';
+import './home.css'; // Certifique-se de ter um arquivo CSS para estilizar a página
 
 const Home = () => {
-  const [username, setUsername] = useState('John Doe');
-  const [balance, setBalance] = useState(1000);
+  const [user, setUser] = useState({
+    username: 'John Doe',
+    balance: 1000,
+  });
+
   const [depositAmount, setDepositAmount] = useState('');
   const [withdrawAmount, setWithdrawAmount] = useState('');
   const [transferAmount, setTransferAmount] = useState('');
   const [transferTo, setTransferTo] = useState('');
 
-  const handleDeposit = () => {
+  const [showDeposit, setShowDeposit] = useState(false);
+  const [showWithdraw, setShowWithdraw] = useState(false);
+  const [showTransfer, setShowTransfer] = useState(false);
+
+  const handleDeposit = (e) => {
+    e.stopPropagation(); // Impede a propagação do evento para o elemento pai
     const amount = parseFloat(depositAmount);
     if (!isNaN(amount)) {
-      setBalance(balance + amount);
+      setUser({
+        ...user,
+        balance: user.balance + amount,
+      });
       setDepositAmount('');
     }
   };
 
-  const handleWithdraw = () => {
+  const handleWithdraw = (e) => {
+    e.stopPropagation(); // Impede a propagação do evento para o elemento pai
     const amount = parseFloat(withdrawAmount);
-    if (!isNaN(amount) && amount <= balance) {
-      setBalance(balance - amount);
+    if (!isNaN(amount) && amount <= user.balance) {
+      setUser({
+        ...user,
+        balance: user.balance - amount,
+      });
       setWithdrawAmount('');
     }
   };
 
-  const handleTransfer = () => {
+  const handleTransfer = (e) => {
+    e.stopPropagation(); // Impede a propagação do evento para o elemento pai
     const amount = parseFloat(transferAmount);
-    if (!isNaN(amount) && amount <= balance) {
-      setBalance(balance - amount);
+    if (!isNaN(amount) && amount <= user.balance) {
+      setUser({
+        ...user,
+        balance: user.balance - amount,
+      });
       setTransferAmount('');
       setTransferTo('');
     }
   };
 
   return (
-    <div>
-      <h2>Informações da Conta</h2>
-      <p>Usuário: {username}</p>
-      <p>Saldo: R${balance.toFixed(2)}</p>
+    <div className="home-container">
+      <div className="card">
+        <h2>Informações da Conta</h2>
+        <p>Usuário: {user.username}</p>
+        <p>Saldo: R${user.balance.toFixed(2)}</p>
+      </div>
 
-      <h3>Depósito</h3>
-      <input
-        type="text"
-        value={depositAmount}
-        onChange={(e) => setDepositAmount(e.target.value)}
-        placeholder="Digite o valor do depósito"
-      />
-      <Botao texto='Depositar' funcao={handleDeposit}/>
+      <div className="card">
+        <div className="action-card" onClick={() => setShowDeposit(!showDeposit)}>
+          <h3>Depósito</h3>
+          {showDeposit && (
+            <div onClick={(e) => e.stopPropagation()}>
+              <input
+                type="text"
+                value={depositAmount}
+                onChange={(e) => setDepositAmount(e.target.value)}
+                placeholder="Digite o valor do depósito"
+              />
+              <button onClick={handleDeposit}>Depositar</button>
+            </div>
+          )}
+        </div>
 
-      <h3>Saque</h3>
-      <input
-        type="text"
-        value={withdrawAmount}
-        onChange={(e) => setWithdrawAmount(e.target.value)}
-        placeholder="Digite o valor do saque"
-      />
-      <Botao texto='Sacar' funcao={handleWithdraw}/>
+        <div className="action-card" onClick={() => setShowWithdraw(!showWithdraw)}>
+          <h3>Saque</h3>
+          {showWithdraw && (
+            <div onClick={(e) => e.stopPropagation()}>
+              <input
+                type="text"
+                value={withdrawAmount}
+                onChange={(e) => setWithdrawAmount(e.target.value)}
+                placeholder="Digite o valor do saque"
+              />
+              <button onClick={handleWithdraw}>Sacar</button>
+            </div>
+          )}
+        </div>
 
-      <h3>Transferência</h3>
-      <input
-        type="text"
-        value={transferAmount}
-        onChange={(e) => setTransferAmount(e.target.value)}
-        placeholder="Digite o valor da transferência"
-      />
-      <input
-        type="text"
-        value={transferTo}
-        onChange={(e) => setTransferTo(e.target.value)}
-        placeholder="Digite o usuário do destinatário"
-      />
-      <Botao texto='Transferir' funcao={handleTransfer}/>
+        <div className="action-card" onClick={() => setShowTransfer(!showTransfer)}>
+          <h3>Transferência</h3>
+          {showTransfer && (
+            <div onClick={(e) => e.stopPropagation()}>
+              <input
+                type="text"
+                value={transferAmount}
+                onChange={(e) => setTransferAmount(e.target.value)}
+                placeholder="Digite o valor da transferência"
+              />
+              <input
+                type="text"
+                value={transferTo}
+                onChange={(e) => setTransferTo(e.target.value)}
+                placeholder="Digite o usuário do destinatário"
+              />
+              <button onClick={handleTransfer}>Transferir</button>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
--- Front-end/src/components/LoginForm/index.jsx
+++ Front-end/src/components/LoginForm/index.jsx
@@ -1,32 +1,66 @@
-import CampoTexto from "../campoTexto"
-import Botao from "../Botao"
-import RedesSocial from "../redesSocial"
-import "./loginForm.css"
-import { Link, useNavigate } from "react-router-dom"
+import React, { useState } from "react";
+import CampoTexto from "../campoTexto";
+import Botao from "../Botao";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const LoginForm = () => {
-    const navigate = useNavigate()
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        
-        try {
-            navigate('/home')
-        } catch (error) {
-            console.log(error);
-        }
-        
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post("http://192.168.1.117:6002/login", {
+        username,
+        password,
+      });
+
+      if (response.status === 200) {
+        // Login bem-sucedido
+        navigate("/home");
+      } else {
+        // Trate outros casos de resposta aqui
+      }
+    } catch (error) {
+      console.error("Erro no login:", error);
+
+      if (error.response && error.response.status === 401) {
+        // Credenciais inválidas
+        // Exibir mensagem para o usuário informando credenciais inválidas
+        alert("Credenciais inválidas. Tente novamente.");
+      } else {
+        // Outro tipo de erro
+        // Trate conforme necessário
+        alert("Erro durante o login. Tente novamente mais tarde.");
+      }
     }
-    return ( 
-        <div className="loginForm">
-            <h2>Entrar</h2>
-            <CampoTexto className="CampoEmail" type="text" placeholder="Digite o seu E-mail"/>
-            <CampoTexto className="CampoSenha" type="password" placeholder="Digite a sua senha"/>
-            <a><Link to="/cadastro">Esqueceu sua senha</Link></a>
-            <Botao funcao={handleSubmit} texto="Entrar"/>
-           
-            
-        </div>
-     );
-}
- 
+    console.log(username, password);
+  };
+
+  return (
+    <div className="loginForm">
+      <h2>Entrar</h2>
+      <CampoTexto
+        className="CampoEmail"
+        type="text"
+        placeholder="Digite o seu E-mail"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <CampoTexto
+        className="CampoSenha"
+        type="password"
+        placeholder="Digite a sua senha"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Link to="/cadastro">Esqueceu sua senha</Link>
+      <Botao funcao={handleSubmit} texto="Entrar" />
+    </div>
+  );
+};
+
 export default LoginForm;
